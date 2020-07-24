828bf5fbdd20b2ca600a9463a1018951a4d458d5
Biruk Kebede <birukgirma11@gmail.com>
2020-07-24 14:01:53 -0400

    Updated Apple YouTube API key

--- src/Components/Youtube/Youtube.js
+++ src/Components/Youtube/Youtube.js
@@ -1,76 +1,77 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 //https://www.googleapis.com/youtube/v3/search?key=AIzaSyBKNveVpH6bsHyKC-iKrcVAeZ3zwlwzRPw&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=2
 
 class Youtube extends Component {
+  constructor() {
+    super();
+    this.state = {
+      youTubeVideos: [],
+    };
+  }
 
-	constructor(){
-		super();
-		this.state = {
-			youTubeVideos: []
-		}
-	}
+  componentDidMount() {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDr5hAYICU77mr03xv_MpwfJFF6dk_TKTM&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=6"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const youTubeVideos = data.items;
+        this.setState((state) => {
+          return {
+            youTubeVideos: youTubeVideos,
+          };
+        });
+      });
+  }
 
-	componentDidMount(){
-		fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCW5JQsiZXFXfNBol-lYr7eRGXckUYmhmQ&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=6")
-			.then(response => response.json())
-			.then((data) => {
-				const youTubeVideos = data.items;
-				this.setState((state) => {
-			        return {
-			          youTubeVideos: youTubeVideos,
-			        };
-			      }
-				);
-			})
-	}
-	
+  render() {
+    // console.log(this.state.youTubeVideos);
+    return (
+      <section className="youtubeVideosWrapper">
+        <div className="allVideosWrapper">
+          <div className="container">
+            <div className="row justify-content-center text-center">
+              <div className="col-12">
+                <div className="title-wraper">
+                  Latest Videos <br />
+                  <br />
+                </div>
+              </div>
+              {this.state.youTubeVideos.map((singleVideo, i) => {
+                let vidId = singleVideo.id.videoId;
+                let vidLink = `https://www.youtube.com/watch?v=${vidId}`;
+                let videoWrapper = (
+                  <div key={i} className="col-sm-12 col-md-6 col-lg-4">
+                    <div className="singleVideoWrapper">
+                      <div className="videoThumbnail">
+                        <a href={vidLink} target="_blank">
+                          <img src={singleVideo.snippet.thumbnails.high.url} />
+                        </a>
+                      </div>
+                      <div className="videoInfoWrapper">
+                        <div className="videoTitle">
+                          <a href={vidLink} target="_blank">
+                            {singleVideo.snippet.title}
+                          </a>
+                        </div>
+                        <div className="videoDesc">
+                          {singleVideo.snippet.description}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+                return videoWrapper;
+              })}
 
-	render () {
-
-		// console.log(this.state.youTubeVideos);
-		return (
-			<section className="youtubeVideosWrapper">
-				<div className="allVideosWrapper">
-					<div className="container">
-						<div className="row justify-content-center text-center"> 
-							<div className="col-12">
-								<div className="title-wraper">
-									Latest Videos <br/><br/>
-								</div>
-							</div> 
-								{
-									this.state.youTubeVideos.map((singleVideo, i) => {
-										let vidId = singleVideo.id.videoId; 
-										let vidLink = `https://www.youtube.com/watch?v=${vidId}`;
-										let videoWrapper =
-										<div key={i} className="col-sm-12 col-md-6 col-lg-4">
-											<div className="singleVideoWrapper">
-												<div className="videoThumbnail">
-													<a href={vidLink} target="_blank">
-														<img src={singleVideo.snippet.thumbnails.high.url} />
-													</a>
-												</div>
-												<div className="videoInfoWrapper">
-													<div className="videoTitle"><a href={vidLink} target="_blank">{singleVideo.snippet.title}</a></div>			
-													<div className="videoDesc">{singleVideo.snippet.description}</div>
-												</div>
-											</div>
-										</div>;
-										return videoWrapper; 
-									})
-								}
-
-								{this.videoWrapper}
-
-						</div>
-					</div>
-				</div>
-			</section>
-
-		);
-	}
+              {this.videoWrapper}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Youtube;
